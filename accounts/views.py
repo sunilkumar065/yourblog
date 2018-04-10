2649d4d08f3d08bf14eafca4d3f75e0fd5831c1a
Sunil Kumar <s55.pragya@gmail.com>
2018-04-10 17:09:17 +0530

    fixes

--- accounts/views.py
+++ accounts/views.py
@@ -22,24 +22,24 @@ class RegistrationView(APIView):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            dashboard_url = reverse('dashboard',kwargs={'pk':user.id})
-            return HttpResponseRedirect(dashboard_url)
+            return Response(serializer.data)
         else:
             return Response(serializer.errors)
 
 class LoginView(APIView):
     def post(self,request):
+        import ipdb; ipdb.set_trace()
         email = request.data.get('email',None)
         password = request.data.get('password',None)
         if email and password:
             user = authenticate(request,username=email,password=password)
             if user:
                 login(request,user)
-                return Response({'logged_in':True})
+                return Response({'success':'True','msg':'logged_in'})
             else:
-                return Response({'user':False})
+                return Response({'success':'False','msg':'no such user'})
         else:
-            return Reponse({'sucess':False})
+            return Response({'success':'False','msg':'no such user'})
 
 def logout_view(request):
     logout(request)
